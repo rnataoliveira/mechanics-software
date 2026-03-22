1da75ffa0aeaeb34a1bc1e532776b243adc4271f
JR Oliveira <renatabels@gmail.com>
2026-03-22 19:23:11 -0300

    refactor(tests): align CreateVehicleUseCaseTests with project test pattern

--- tests/MechanicsSoftware.UnitTests/Application/Vehicles/CreateVehicleUseCaseTests.cs
+++ tests/MechanicsSoftware.UnitTests/Application/Vehicles/CreateVehicleUseCaseTests.cs
@@ -14,9 +14,17 @@ namespace MechanicsSoftware.UnitTests.Application.Vehicles;
 public class CreateVehicleUseCaseTests
 {
     private static readonly Guid CustomerId = Guid.NewGuid();
+    private const string ValidName  = "Joel Silva";
+    private const string ValidCpf   = "529.982.247-25";
+    private const string ValidEmail = "joel@email.com";
+    private const string ValidPhone = "11999999999";
+    private const string ValidPlate = "ABC1234";
+    private const string ValidMake  = "Toyota";
+    private const string ValidModel = "Corolla";
+    private const int    ValidYear  = 2020;
 
     private static Customer BuildCustomer(Guid? id = null) =>
-        Customer.Create(id ?? CustomerId, "Joel Silva", "52998224725", PersonType.INDIVIDUAL, "joel@email.com", "11999999999");
+        Customer.Create(id ?? CustomerId, ValidName, ValidCpf, PersonType.INDIVIDUAL, ValidEmail, ValidPhone);
 
     private static (Mock<IAppDbContext> db, Mock<DbSet<Customer>> customers, Mock<DbSet<Vehicle>> vehicles)
         BuildContext(List<Customer>? customers = null, List<Vehicle>? vehicles = null)
@@ -39,14 +47,14 @@ public class CreateVehicleUseCaseTests
         var (db, _, mockVehicles) = BuildContext(customers: [customer]);
 
         var useCase = new CreateVehicleUseCase(db.Object);
-        var request = new CreateVehicleRequest("ABC1234", "Toyota", "Corolla", 2020, CustomerId);
+        var request = new CreateVehicleRequest(ValidPlate, ValidMake, ValidModel, ValidYear, CustomerId);
 
         var result = await useCase.ExecuteAsync(request);
 
-        result.LicensePlate.Should().Be("ABC1234");
-        result.Make.Should().Be("Toyota");
-        result.Model.Should().Be("Corolla");
-        result.Year.Should().Be(2020);
+        result.LicensePlate.Should().Be(ValidPlate);
+        result.Make.Should().Be(ValidMake);
+        result.Model.Should().Be(ValidModel);
+        result.Year.Should().Be(ValidYear);
         result.CustomerId.Should().Be(CustomerId);
         mockVehicles.Verify(m => m.Add(It.IsAny<Vehicle>()), Times.Once);
         db.Verify(d => d.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
@@ -58,7 +66,7 @@ public class CreateVehicleUseCaseTests
         var (db, _, _) = BuildContext();
 
         var useCase = new CreateVehicleUseCase(db.Object);
-        var request = new CreateVehicleRequest("ABC1234", "Toyota", "Corolla", 2020, CustomerId);
+        var request = new CreateVehicleRequest(ValidPlate, ValidMake, ValidModel, ValidYear, CustomerId);
 
         var act = async () => await useCase.ExecuteAsync(request);
 
@@ -69,15 +77,15 @@ public class CreateVehicleUseCaseTests
     public async Task ExecuteAsync_DuplicatePlate_ThrowsDomainException()
     {
         var customer = BuildCustomer();
-        var existingVehicle = Vehicle.Create(Guid.NewGuid(), new LicensePlate("ABC1234"), "Honda", "Civic", 2019, CustomerId);
+        var existingVehicle = Vehicle.Create(Guid.NewGuid(), new LicensePlate(ValidPlate), "Honda", "Civic", 2019, CustomerId);
         var (db, _, _) = BuildContext(customers: [customer], vehicles: [existingVehicle]);
 
         var useCase = new CreateVehicleUseCase(db.Object);
-        var request = new CreateVehicleRequest("ABC1234", "Toyota", "Corolla", 2020, CustomerId);
+        var request = new CreateVehicleRequest(ValidPlate, ValidMake, ValidModel, ValidYear, CustomerId);
 
         var act = async () => await useCase.ExecuteAsync(request);
 
-        await act.Should().ThrowAsync<DomainException>().WithMessage("*ABC1234*");
+        await act.Should().ThrowAsync<DomainException>().WithMessage($"*{ValidPlate}*");
     }
 
     [Fact]
@@ -87,7 +95,7 @@ public class CreateVehicleUseCaseTests
         var (db, _, _) = BuildContext(customers: [customer]);
 
         var useCase = new CreateVehicleUseCase(db.Object);
-        var request = new CreateVehicleRequest("abc1A23", "Toyota", "Corolla", 2020, CustomerId);
+        var request = new CreateVehicleRequest("abc1A23", ValidMake, ValidModel, ValidYear, CustomerId);
 
         var result = await useCase.ExecuteAsync(request);
 
@@ -101,7 +109,7 @@ public class CreateVehicleUseCaseTests
         var (db, _, _) = BuildContext(customers: [customer]);
 
         var useCase = new CreateVehicleUseCase(db.Object);
-        var request = new CreateVehicleRequest("INVALID", "Toyota", "Corolla", 2020, CustomerId);
+        var request = new CreateVehicleRequest("INVALID", ValidMake, ValidModel, ValidYear, CustomerId);
 
         var act = async () => await useCase.ExecuteAsync(request);
 
